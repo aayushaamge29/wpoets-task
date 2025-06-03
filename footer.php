b39c33670db9afb085d26628d2965ae66d295e4b
aayushaamge29 <aayushaamge123@gmail.com>
2025-06-03 15:51:42 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Header</title>
+  <title>Footer</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   <style>
     .site-footer {
